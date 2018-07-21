c56c501fa949e39115f25df4e244e0ea149e4a05
Wilson <wilson@shiyideMBP.hsd1.ca.comcast.net>
2018-07-21 14:36:06 -0700

    Initial commit.

---
+++ index.js
@@ -0,0 +1,5 @@
+import './App/Config/ReactotronConfig'
+import { AppRegistry } from 'react-native'
+import App from './App/Containers/App'
+
+AppRegistry.registerComponent('smart_checkin', () => App)
